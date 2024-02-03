853768e9a50af629c468c5c7b0939980988306ff
rchelbii <ryadhchalbi0@gmail.com>
2024-02-03 01:17:18 +0100

    refactoring - using Terminal mod

--- src/editor.rs
+++ src/editor.rs
@@ -1,9 +1,8 @@
-use std::io::{self, stdout, Write};
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
 use crate::Terminal;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
@@ -12,7 +11,6 @@ pub struct Editor {
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
         loop {
             if let Err(error) = self.refresh_screen() {
                 die(error);
@@ -27,14 +25,17 @@ impl Editor {
     }
     
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));            
+        Terminal::cursor_hide();
+        Terminal::cursor_position(1, 1);
         if self.should_quit {
+            Terminal::clear_screen();
             println!("GOODBYE! \r");
         } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1, 1));            
+            Terminal::cursor_position(1, 1);
         }
-        io::stdout().flush()
+        Terminal::cursor_show();
+        Terminal::flush()
     }
 
     pub fn default() -> Self {
@@ -45,7 +46,7 @@ impl Editor {
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
             _ => (),
@@ -54,16 +55,16 @@ impl Editor {
     }
     
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height {
-            println!(".\r");
-        }
-    }
-}
-
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
+        let height = self.terminal.size().height - 1; 
+        let width = self.terminal.size().width - 1; 
+        for row in 0..height {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                Terminal::cursor_position(row, width / 3); 
+                println!("Edito -- the editor {}\r", VERSION);
+            } else {
+                println!(".\r");
+            }
         }
     }
 }
