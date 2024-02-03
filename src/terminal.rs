a99c3284fbcfd382cd975100208e6c6a6c6e0cee
rchelbii <ryadhchalbi0@gmail.com>
2024-02-03 01:16:09 +0100

    move terminal related functionalities from editor.rs to terminal.rs

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,24 +1,65 @@
+use std::io::{self, stdout, Write};
+use termion::input::TermRead;
+use termion::event::Key;
+use termion::raw::{IntoRawMode, RawTerminal};
+
 pub struct Size {
     pub width: u16,
     pub height: u16,
-};
+}
 
 pub struct Terminal {
-    pub size: Size,
-};
+    size: Size,
+    _stdout: RawTerminal<std::io::Stdout>,
+}
 
 impl Terminal {
-    pub fn default(&self) -> Result<Self, std::io::Error> {
+    pub fn default() -> Result<Self, std::io::Error> {
         let size = termion::terminal_size()?;
         Ok(Self {
-            size: {
+            size: Size {
                 width: size.0,
                 height: size.1,
             },
+            _stdout: stdout().into_raw_mode()?,
         })
     }
 
-    pub fn size(&slef) -> &Size {
+    pub fn size(&self) -> &Size {
         &self.size
+    }
+
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);            
+    }
+    
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);            
+    }
+
+    pub fn cursor_position(x: u16, y:u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+        print!("{}", termion::cursor::Goto(x, y));            
+    }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+    
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+    
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
     }
 }
