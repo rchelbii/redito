de5add21e9f7c5ec7f8398107753e2063ebf5480
rchelbii <ryadhchalbi0@gmail.com>
2024-02-05 19:56:41 +0100

    add fn len(&self), fn is_empty(&self) to row.rs

--- src/row.rs
+++ src/row.rs
@@ -16,4 +16,12 @@ impl Row {
         let start = cmp::min(start, end);
         self.string.get(start..end).unwrap_or_default().to_string()
     }
+
+    pub fn len(&self) -> usize {
+        self.string.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.string.is_empty()
+    }
 }
