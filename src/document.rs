3d0d60be2134f724c50433b2d86635c23d381384
rchelbii <ryadhchalbi0@gmail.com>
2024-02-11 13:50:47 +0100

    modify Struct Document {...} to register file name -- update fn open(...) to handle file_name of the document

--- src/document.rs
+++ src/document.rs
@@ -5,16 +5,20 @@ use std::fs;
 #[derive(Default)]
 pub struct Document {
     rows: Vec<Row>,
+    pub file_name: Option<String>,
 }
 
 impl Document {
-    pub fn open(file_name: &str) -> Result<Self, std::io::Error> {
-        let contents = fs::read_to_string(file_name)?;
+    pub fn open(filename: &str) -> Result<Self, std::io::Error> {
+        let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
         for value in contents.lines() {
             rows.push(Row::from(value));
         }
-        Ok(Self { rows })
+        Ok(Self { 
+            rows,
+            file_name: Some(filename.to_string()),
+        })
     }
 
     pub fn row(&self, index: usize) -> Option<&Row> {
