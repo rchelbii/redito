9cecc6bff1df546d025684ff94be5e02d2edc03c
rchelbii <ryadhchalbi0@gmail.com>
2024-02-11 13:46:02 +0100

    add functions to handle FG

--- src/terminal.rs
+++ src/terminal.rs
@@ -77,4 +77,12 @@ impl Terminal {
     pub fn reset_bg_color() {
         print!("{}", color::Bg(color::Reset));
     }
+
+    pub fn set_fg_color(color: color::Rgb) {
+        print!("{}", color::Fg(color));
+    }
+    
+    pub fn reset_fg_color() {
+        print!("{}", color::Fg(color::Reset));
+    }
 }
