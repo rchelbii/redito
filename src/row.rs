0d2efc258327876f6305ab08484ac31d08e3711e
rchelbii <ryadhchalbi0@gmail.com>
2024-02-13 23:37:42 +0100

    add fn append(...)

--- src/row.rs
+++ src/row.rs
@@ -76,6 +76,11 @@ impl Row {
         self.update_len();
     }
 
+    pub fn append(&mut self, new: &Self) {
+        self.string = format!("{}{}", self.string, new.string);
+        self.update_len();
+    }
+
     pub fn len(&self) -> usize {
         self.len
     }
