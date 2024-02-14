63a55eb77e4e26dc545d5e96c8f604c9607b6451
rchelbii <ryadhchalbi0@gmail.com>
2024-02-14 23:15:46 +0100

    handle inserting new line in the document

--- src/document.rs
+++ src/document.rs
@@ -22,7 +22,23 @@ impl Document {
         })
     }
 
+    fn insert_newline(&mut self, at: &Position) {
+        if at.y > self.len() {
+            return;
+        }
+        let new_row = Row::default();
+        if at.y == self.len() || at.y.saturating_add(1) == self.len() {
+            self.rows.push(new_row);
+        } else {
+            self.rows.insert(at.y + 1, new_row);
+        }
+    }
+
     pub fn insert(&mut self, at: &Position, c: char) {
+        if c == '\n' {
+            self.insert_newline(at);
+            return;
+        }
         if at.y == self.len() {
             let mut row = Row::default();
             row.insert(0, c);
