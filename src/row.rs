070cc0a5d9cab9e9391f24af6b12d5e8b337c489
rchelbii <ryadhchalbi0@gmail.com>
2024-02-15 19:47:07 +0100

    add fn split(...) -- split row into to parts at specific position

--- src/row.rs
+++ src/row.rs
@@ -81,6 +81,20 @@ impl Row {
         self.update_len();
     }
 
+    pub fn split(&mut self, at: usize) -> Self {
+        let mut beginning: String = self.string[..]
+            .graphemes(true)
+            .take(at)
+            .collect();
+        let remainder: String = self.string[..]
+            .graphemes(true)
+            .skip(at)
+            .collect();
+        self.string = beginning;
+        self.update_len();
+        Self::from(&remainder[..])
+    }
+
     pub fn len(&self) -> usize {
         self.len
     }
