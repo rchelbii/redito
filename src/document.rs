0470e1ccd1e59192a1aaba256d91a2ff8b144e7c
rchelbii <ryadhchalbi0@gmail.com>
2024-02-16 00:11:47 +0100

    add fn save(...) -- handle file saving to disk

--- src/document.rs
+++ src/document.rs
@@ -2,6 +2,7 @@ use crate::Row;
 use crate::Position;
 
 use std::fs;
+use std::io::{Error, Write};
 
 #[derive(Default)]
 pub struct Document {
@@ -62,6 +63,17 @@ impl Document {
             let row = self.rows.get_mut(at.y).unwrap();
             row.delete(at.x);
         }
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        if let Some(file_name) = &self.file_name {
+            let mut file = fs::File::create(file_name)?;
+            for row in &self.rows {
+                file.write_all(row.as_bytes())?;
+                file.write_all(b"\n")?;
+            }
+        }
+        Ok(())
     }
 
     pub fn row(&self, index: usize) -> Option<&Row> {
